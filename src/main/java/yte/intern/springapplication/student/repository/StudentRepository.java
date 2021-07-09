3658f8573738716b6b2bd4e237a1edb6186cb734
fatih.dogmus <fdogmus06@gmail.com>
2021-07-09 10:42:48 +0300

    Eklenen öğrencinin var olması halinde hata mesjaı dönülmesi sağlandı

--- src/main/java/yte/intern/springapplication/student/repository/StudentRepository.java
+++ src/main/java/yte/intern/springapplication/student/repository/StudentRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import yte.intern.springapplication.student.entity.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    boolean existsByStudentNumber(String studentNumber);
 }
--- src/main/java/yte/intern/springapplication/student/service/StudentServie.java
+++ src/main/java/yte/intern/springapplication/student/service/StudentServie.java
@@ -9,16 +9,25 @@ import yte.intern.springapplication.student.repository.StudentRepository;
 @Service
 public class StudentServie {
 
-    private final StudentRepository studentRepository;
     private static final String STUDENT_ADDED_MESSAGE = "Student with student number %s has been added successfully";
+    private static final String STUDENT_ALREADY_EXISTS_MESSAGE = "Student with student number %s already exists!";
+
+    private final StudentRepository studentRepository;
 
     public StudentServie(final StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
     public MessageResponse addStudent(final Student newStudent) {
+        if (studentRepository.existsByStudentNumber(newStudent.studentNumber())) {
+            return new MessageResponse(MessageType.ERROR, studentAlreadyExistsMessage(newStudent.studentNumber()));
+        }
         studentRepository.save(newStudent);
         return new MessageResponse(MessageType.SUCCESS, studentAddedMessage(newStudent.studentNumber()));
+    }
+
+    private String studentAlreadyExistsMessage(final String studentNumber) {
+        return STUDENT_ALREADY_EXISTS_MESSAGE.formatted(studentNumber);
     }
 
     private String studentAddedMessage(final String studentNumber) {
