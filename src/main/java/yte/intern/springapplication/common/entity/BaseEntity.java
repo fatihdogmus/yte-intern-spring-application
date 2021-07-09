16e5d2380f5779b0a6403a355997d10bc49e9a86
fatih.dogmus <fdogmus06@gmail.com>
2021-07-09 09:27:48 +0300

    BaseEntity'e ortak olan field'lar eklendi

--- src/main/java/yte/intern/springapplication/common/entity/BaseEntity.java
+++ src/main/java/yte/intern/springapplication/common/entity/BaseEntity.java
@@ -1,17 +1,29 @@
 package yte.intern.springapplication.common.entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Version
+    private Long version;
+
+    @CreatedDate
+    private LocalDateTime creationDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 
     @Override
     public boolean equals(final Object o) {
