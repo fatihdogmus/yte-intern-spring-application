79a3845cff25e20f764e74d64bf0f4634d8d62e5
fatihdogmus <fdogmus06@gmail.com>
2022-09-05 10:54:19 +0300

    Base entity'e equals ve hash code eklendi

--- src/main/java/yte/intern/springapplication/common/entity/BaseEntity.java
+++ src/main/java/yte/intern/springapplication/common/entity/BaseEntity.java
@@ -1,11 +1,13 @@
 package yte.intern.springapplication.common.entity;
 
 import lombok.Getter;
+import org.hibernate.proxy.HibernateProxy;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.util.Objects;
 
 @Getter
 @MappedSuperclass
@@ -14,4 +16,31 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (obj instanceof HibernateProxy) {
+            obj = ((HibernateProxy) obj).getHibernateLazyInitializer().getImplementation();
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BaseEntity other = (BaseEntity) obj;
+        if (this.id == null) {
+            return false;
+        } else {
+            return this.id.equals(other.id);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
