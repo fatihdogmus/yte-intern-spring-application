4610b0cdcfd7af2e986f23b81298dc8577ca3636
fatihdogmus <fdogmus06@gmail.com>
2022-09-05 14:21:35 +0300

    Student'ların listelenebilmesi sağlandı

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,39 @@
+import {Box} from "@mui/material";
+import {DataGrid} from '@mui/x-data-grid';
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const columns = [
+  {field: 'id', headerName: 'ID', width: 90},
+  {field: 'name', headerName: 'Name', width: 90},
+  {field: 'surname', headerName: 'Surname', width: 90},
+  {field: 'tcKimlikNo', headerName: 'TC Kimlik No', width: 90},
+  {field: 'studentNumber', headerName: 'Student Number', width: 90},
+  {field: 'email', headerName: 'E-Mail', width: 90},
+];
+
 function App() {
+
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    axios.get("/students")
+      .then(response => setStudents(response.data))
+  }, []);
+
   return (
     <div>
-      Hebele
+      <Box sx={{height: 400, width: '100%'}}>
+        <DataGrid
+          rows={students}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          disableSelectionOnClick
+          experimentalFeatures={{newEditingApi: true}}
+        />
+      </Box>
     </div>
   );
 }
