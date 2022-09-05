383a51dcc18875ac135e388b02ca6d0c5be71e8c
fatihdogmus <fdogmus06@gmail.com>
2022-09-05 11:08:37 +0300

    BaseEntity'e version created date ve last modified date eklendi

--- src/main/java/yte/intern/springapplication/common/entity/BaseEntity.java
+++ src/main/java/yte/intern/springapplication/common/entity/BaseEntity.java
@@ -2,20 +2,31 @@ package yte.intern.springapplication.common.entity;
 
 import lombok.Getter;
 import org.hibernate.proxy.HibernateProxy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
+
+    @Version
+    private Long version;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 
     @Override
     public boolean equals(Object obj) {
